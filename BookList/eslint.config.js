d45d4858bf7f6721e19cf29593e2e7db0e097285
Michael <silvamike1710@gmail.com>
2026-03-30 18:34:03 -0300

    yet more lint stuff

--- BookList/eslint.config.js
+++ BookList/eslint.config.js
@@ -1,10 +1,18 @@
-export default defineConfig([
-  globalIgnores(['dist']),
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: ['dist'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -16,4 +24,4 @@ export default defineConfig([
       "react-hooks/set-state-in-effect": "off"
     }
   },
-])
+]

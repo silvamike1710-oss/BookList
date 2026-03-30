74af39162f349721c9c28a24eeba31bf46e5ffc5
Michael <silvamike1710@gmail.com>
2026-03-30 17:46:51 -0300

    fixed lint error

--- BookList/vite.config.ts
+++ BookList/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/BookList/',  
-  // @ts-ignore
+  // @ts-expect-error
   test: {
     globals: true,
     environment: 'jsdom',
